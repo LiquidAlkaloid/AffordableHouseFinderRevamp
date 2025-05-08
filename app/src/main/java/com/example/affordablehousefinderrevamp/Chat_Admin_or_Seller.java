c7ca67d9fc0fa93d2621e985d088b18300fb8d69
LiquidAlkaloid <jantheodorebugayong@gmail.com>
2025-05-08 23:48:18 +0900

    New latest1 commit

--- app/src/main/java/com/example/affordablehousefinderrevamp/Chat_Admin_or_Seller.java
+++ app/src/main/java/com/example/affordablehousefinderrevamp/Chat_Admin_or_Seller.java
@@ -8,14 +8,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Homepage extends AppCompatActivity {
+public class Chat_Admin_or_Seller extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_homepage);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rootLayout), (v, insets) -> {
+        setContentView(R.layout.activity_chat_admin_or_seller);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
